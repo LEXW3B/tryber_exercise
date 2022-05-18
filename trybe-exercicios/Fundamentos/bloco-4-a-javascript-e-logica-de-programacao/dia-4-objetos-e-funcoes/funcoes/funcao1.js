b6f689af4f35f3110a3f94daa3f9ccd17845e918
Alexandre <alexmanson12345@gmail.com>
2022-05-17 21:44:08 -0300

    Finalizado o exercício de funcoes normais

---
+++ trybe-exercicios/Fundamentos/bloco-4-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/funcoes/funcao1.js
@@ -0,0 +1,19 @@
+//exercicio 07 do lecture/4.1
+function percent(a){
+    if(a<0 || a>100){
+        console.log("ERROR");
+    }else if(a>=90){
+        console.log("A");
+    }else if(a>=80){
+        console.log("B");
+    }else if(a>=70){
+        console.log("C");
+    }else if(a>=60){
+        console.log("D");
+    }else if(a>=50){
+        console.log("E");
+    }else{console.log
+        console.log("F");
+    }
+}
+percent(90);
