ab72c42e8c9a797e04848a0700bac860591baaa3
Alexandre <alexmanson12345@gmail.com>
2022-05-21 09:55:32 -0300

    manipulação do DOM por ID getElementById

---
+++ trybe-exercicios/Fundamentos/bloco-5-dom-eventos-e-web-storage/dia-1-dom-eventos-e-web-storage/exercícios comum/manipulado o DOM/script.js
@@ -0,0 +1,11 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+let titulo = document.getElementById('page-title');
+titulo.innerText = 'Stargate';
+
+let paragrafo = document.getElementById('paragraph');
+paragrafo.style = 'background-color: green; color: red;';
+
+let subT = document.getElementById('subtitle');
+subT.innerText = 'A série e melhor que o filme.';
