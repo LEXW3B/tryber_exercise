f25e8bd936fc1151eb30cd19f8c4661d2cb6fdfb
Alexandre <alexmanson12345@gmail.com>
2022-05-21 10:31:50 -0300

    exercício de manipulação de DOM por getElementsByTagName concluído.

---
+++ trybe-exercicios/Fundamentos/bloco-5-dom-eventos-e-web-storage/dia-1-dom-eventos-e-web-storage/exercícios comum/classe/script.js
@@ -0,0 +1,7 @@
+let paragrafo = document.getElementsByClassName('paragraph');
+paragrafo[0].style = 'color:red;';
+
+let subT = document.getElementsByTagName('h4');
+subT[0].style = 'color: green;';
+
+
