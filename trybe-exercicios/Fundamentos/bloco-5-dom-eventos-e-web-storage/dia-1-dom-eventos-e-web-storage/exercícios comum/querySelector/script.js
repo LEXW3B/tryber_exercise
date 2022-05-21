6603bc3e45a0cf20984b592fcd8b0d89b81efa74
Alexandre <alexmanson12345@gmail.com>
2022-05-21 11:42:42 -0300

    ATUALIZAR todos os exercícios para serem feitos com querySelector feito.

---
+++ trybe-exercicios/Fundamentos/bloco-5-dom-eventos-e-web-storage/dia-1-dom-eventos-e-web-storage/exercícios comum/querySelector/script.js
@@ -0,0 +1,12 @@
+let allP = document.querySelectorAll('p');
+allP.style = 'background-color: red;';
+allP[1].innerText = 'A série e melhor que o filme.';
+
+let p = document.querySelector('.paragraph');
+p.style = 'display: block; color: red;';
+
+let t = document.querySelector('#subtitle');
+t.innerText = 'Stargate';
+
+
+
