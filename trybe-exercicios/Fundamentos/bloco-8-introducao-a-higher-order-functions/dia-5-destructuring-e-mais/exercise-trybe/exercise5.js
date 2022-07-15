752df7cfdf18210c64b8dd392cb1efb8d0bf80ab
Alexandre <alexmanson12345@gmail.com>
2022-07-15 15:10:23 -0300

    Finalizados all of exercise/8.5

---
+++ trybe-exercicios/Fundamentos/bloco-8-introducao-a-higher-order-functions/dia-5-destructuring-e-mais/exercise-trybe/exercise5.js
@@ -0,0 +1,11 @@
+//Escreva a função swap, que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+
+const myList = [1, 2, 3];
+
+// const swap = ([aff, bff, cff]) => [cff, bff, aff];
+// console.log(swap(myList));
+
+//ou
+
+console.log(myList.reverse());
+//nesse caso da certo rsrsrs
